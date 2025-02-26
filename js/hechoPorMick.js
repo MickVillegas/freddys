713f6bb66afd42b70ad95726c2d4e703c3772cd2
MickVillegas <mickprofesional@gmail,com>
2025-02-26 14:10:37 +0100

    he arreglado errores en los formularios, he añadido más feed back y ahora los usuarios pueden cancelar el envio de pedidos o de datos

--- js/hechoPorMick.js
+++ js/hechoPorMick.js
@@ -1,5 +1,5 @@
 let pe = document.getElementsByClassName("pedir");
-  
+  /*
 for(let x = 0; x < pe.length; x++){
   pe[x].addEventListener("click", function() {
     document.getElementById("modal").style.display = "flex";
@@ -8,17 +8,94 @@ for(let x = 0; x < pe.length; x++){
 document.querySelector(".close").addEventListener("click", function() {
   document.getElementById("modal").style.display = "none";
 });
+*/
 
-let en = document.getElementsByClassName("enviar");
-  
-for(let x = 0; x < en.length; x++){
-  en[x].addEventListener("click", function() {
-    document.getElementById("modal").style.display = "flex";
-})
+
+
+
+//botonPulsado.addEventListener("click", enviarDatos)
+for(let x = 0; x < pe.length; x++){
+  pe[x].addEventListener("click", abrirModal)
+}
+
+function abrirModal(){
+  let modal = document.getElementById("modal2")
+  modal.style.display = "flex"
+  modal.addEventListener("click", opcionPulsada)
+}
+
+
+function opcionPulsada(event){
+  let modal = document.getElementById("modal2")
+  let nodoCLick
+  if(event){
+    nodoCLick = event.target
+    if(nodoCLick.id == "aceptar"){
+      modal.style.display = "none"
+      document.getElementById("modal").style.display = "flex";
+      document.querySelector(".close").addEventListener("click", function() {
+      document.getElementById("modal").style.display = "none";
+      });
+    }
+    else{
+      modal.style.display = "none"
+    }
+  }
 }
 
 
 
-document.querySelector(".close").addEventListener("click", function() {
-  document.getElementById("modal").style.display = "none";
-});
+
+
+
+
+
+
+
+let en = document.getElementsByClassName("enviar");
+
+
+//botonPulsado.addEventListener("click", enviarDatos)
+for(let x = 0; x < en.length; x++){
+en[x].addEventListener("click", abrirModal)
+}
+
+function abrirModal(){
+  let modal = document.getElementById("modal2")
+  modal.style.display = "flex"
+  modal.addEventListener("click", opcionPulsada)
+}
+
+
+function opcionPulsada(event){
+  let modal = document.getElementById("modal2")
+  let nodoCLick
+  if(event){
+    nodoCLick = event.target
+    if(nodoCLick.id == "aceptar"){
+      modal.style.display = "none"
+      document.getElementById("modal").style.display = "flex";
+      document.querySelector(".close").addEventListener("click", function() {
+      document.getElementById("modal").style.display = "none";
+      });
+    }
+    else{
+      modal.style.display = "none"
+    }
+  }
+}
+
+/*
+      for(let x = 0; x < en.length; x++){
+        en[x].addEventListener("click", function() {
+          document.getElementById("modal").style.display = "flex";
+      })
+      }
+*/      
+
+
+
+
+  
+
+
